a6097138e2cae880a9458c0287de0b99d65b23f8
chamath pathirana <chamathpathirana20@gmail.com>
2026-07-18 11:13:50 +0530

    gessing game

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("hellow");
+console.log("hii");
+
+let num = Math.random() * 10 + 1;
+
+let randomNum = Math.floor(num);
+
+console.log(randomNum);
+
+function btnAddCustomerOnAction(){
+    let textInput = document.getElementById("inputText 1").value;
+    for (let i = 1; i <= 3; i++){
+        if (randomNum < textInput){
+            let output= document.getElementById("output");
+            output.textContent="higher"
+           
+            }else if(randomNum>textInput){
+                let output= document.getElementById("output");
+                output.textContent="less"
+
+            }else{
+                let output = document.getElementById("output");
+                output.textContent+"win"
+
+            }
+         
+        }
+   }
+
+}
+
+
+
+
+
+
+
